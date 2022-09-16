7a17a81419173596d24d9bfb6de18b1a5e91dc67
Kubi <KubiHUN@users.noreply.github.com>
2022-09-16 08:08:54 +0200

    searrch under UC

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDetails } from "../providers/details";
 import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
   const navigate = useNavigate();
   const { data, getData, setSelectedImage } = useDetails();
+  const [search, setSearch] = useState("");
 
   const nav = (path) => {
     navigate(path);
   };
+
+  // filter data based on search
+  const filteredData = data.filter((pic) =>
+    pic.title.toLowerCase().includes(search.toLowerCase())
+  );
 
   const clickedImage = (id) => {
     setSelectedImage(id);
@@ -22,6 +28,11 @@ const Dashboard = () => {
   return (
     <>
       <div>Dashboard</div>
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => setSearch(e.target.value)}
+      />
       {data.length > 0 &&
         data.map((pic) => (
           <div key={pic.id} onClick={() => clickedImage(pic.id)}>
